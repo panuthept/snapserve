805a4e25da9fa2df3acf7ce553232dbb45b7f115
Panuthep <falcon_270394@hotmail.co.th>
2026-04-21 14:53:16 +0700

    change interface

--- snapserve/loaders.py
+++ snapserve/loaders.py
@@ -2,11 +2,13 @@ from typing import Any
 from snapserve.utils.loaders import load_module
 
 
-def load_attributes(module_path: str, working_dir: str = None) -> dict[str, Any]:
+def load_attributes(module_path: str, expose: str = None, working_dir: str = None) -> dict[str, Any]:
     module = load_module(module_path, working_dir=working_dir)
+
+    expose = expose or ",".join([attr for attr in dir(module) if not attr.startswith("_")])
     
     attributes = {}
-    for attr_name in module_path.split(":")[1].split(","):
+    for attr_name in expose.split(","):
         attr_name = attr_name.strip()
         if not hasattr(module, attr_name):
             raise AttributeError(f"Module '{module.__name__}' does not have an attribute named '{attr_name}'.")
--- snapserve/utils/loaders.py
+++ snapserve/utils/loaders.py
@@ -5,13 +5,9 @@ from types import ModuleType
 
 
 def load_module(module_path: str, working_dir: str = None) -> ModuleType:
-    if ":" not in module_path or module_path.count(":") > 1:
-        raise ValueError(f"Module path '{module_path}' is invalid. It should be in the format 'module_path:variable_name'.")
-    
     # Add the current working directory to sys.path to allow importing from there
     working_dir = working_dir or os.getcwd()
     sys.path.insert(0, working_dir)
     
-    module_path, _ = module_path.split(":")
     module = importlib.import_module(module_path)
     return module
